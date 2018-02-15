85c73854c2a1c380f440bc2b0060f68be8e117f1
Deep Bratt Mondal <deepbratt@gmail.com>
2018-02-15 17:50:03 +0530

    asd
    adasdada

---
+++ metalinks.php
@@ -0,0 +1,23 @@
+<!-- Required meta tags always come first -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+<!-- Main Font -->
+<script src="js/webfontloader.min.js"></script>
+<script>
+	WebFont.load({
+		google: {
+			families: ['Roboto:300,400,500,700:latin']
+		}
+	});
+</script>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+
+<!-- Main Styles CSS -->
+<link rel="stylesheet" type="text/css" href="css/main.min.css">
+<link rel="stylesheet" type="text/css" href="css/fonts.min.css">
