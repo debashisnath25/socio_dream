6c36527fd3f5419ad87a9fbd6f03b09067feb160
debashisnath25 <debashisnath1992@gmail.com>
2018-02-17 18:48:45 +0530

    notification

---
+++ notification.php
@@ -0,0 +1,294 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Your Account - Notifications</title>
+
+	<!-- Required meta tags always come first -->
+	<?php include("metalinks.php");?>
+	<!-- Main Font -->
+	<script src="js/webfontloader.min.js"></script>
+	<script>
+		WebFont.load({
+			google: {
+				families: ['Roboto:300,400,500,700:latin']
+			}
+		});
+	</script>
+
+</head>
+<body>
+
+<?php //include("chatsidebar.php");?>
+
+<?php include("header.php");?>
+<div class="header-spacer"></div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-xl-6 order-xl-2 col-lg-9 order-lg-2 col-md-12 order-md-1 col-sm-12 col-xs-12">
+			<div class="ui-block">
+				<div class="ui-block-title">
+					<h6 class="title">Notifications</h6>
+					<a href="#" class="more"><i class="fas fa-ellipsis-h"></i></a>
+				</div>
+				
+				<!-- Notification List -->
+				
+				<ul class="table-careers">
+					<li class="head" style="padding:5px;">
+						<span>PICTURE</span>
+						<span style="width:300px;">DESCRIPTION</span>
+						<span>DATE POSTED</span>
+					</li>
+					<?php for($i=0;$i<10;$i++){?>
+						<li style="padding:5px;">
+							<span class="position bold"><img src="images/avatar7-sm.jpg" alt="author" style="border-radius:50%"></span>
+							<span class="type bold" style="width:300px;"><a href="#" class="h6 notification-friend">Marina Valentine</a> commented on your new <a href="#" class="notification-link">profile status</a>.</span>
+							<span class="date"><time class="entry-date updated" datetime="2004-07-24T18:18">March 2nd at 10:07am</time></span>
+						</li>
+					<?php } ?>
+				</ul>
+				<!-- ... end Notification List -->
+			</div>
+
+			
+			<!-- Pagination -->
+			
+			<nav aria-label="Page navigation">
+				<ul class="pagination justify-content-center">
+					<li class="page-item disabled">
+						<a class="page-link" href="#" tabindex="-1">Previous</a>
+					</li>
+					<li class="page-item"><a class="page-link" href="#">1<div class="ripple-container"><div class="ripple ripple-on ripple-out" style="left: -10.3833px; top: -16.8333px; background-color: rgb(255, 255, 255); transform: scale(16.7857);"></div></div></a></li>
+					<li class="page-item"><a class="page-link" href="#">2</a></li>
+					<li class="page-item"><a class="page-link" href="#">3</a></li>
+					<li class="page-item"><a class="page-link" href="#">...</a></li>
+					<li class="page-item"><a class="page-link" href="#">12</a></li>
+					<li class="page-item">
+						<a class="page-link" href="#">Next</a>
+					</li>
+				</ul>
+			</nav>
+			<!-- ... end Pagination -->
+		</div>
+
+		<?php include("sidebar.php");?>
+		<?php include("right_sidebar.php");?>
+	</div>
+</div>
+
+<!-- ... end Your Account Personal Information -->
+
+
+
+
+
+<!-- Window-popup-CHAT for responsive min-width: 768px -->
+
+<div class="ui-block popup-chat popup-chat-responsive">
+	<div class="ui-block-title">
+		<span class="icon-status online"></span>
+		<h6 class="title" >Chat</h6>
+		<div class="more">
+			<svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+			<svg class="olymp-little-delete js-chat-open"><use xlink:href="svg-icons/sprites/icons.svg#olymp-little-delete"></use></svg>
+		</div>
+	</div>
+	<div class="mCustomScrollbar">
+		<ul class="notification-list chat-message chat-message-field">
+			<li>
+				<div class="author-thumb">
+					<img src="images/avatar14-sm.jpg" alt="author" class="mCS_img_loaded">
+				</div>
+				<div class="notification-event">
+					<span class="chat-message-item">Hi James! Please remember to buy the food for tomorrow! I’m gonna be handling the gifts and Jake’s gonna get the drinks</span>
+					<span class="notification-date"><time class="entry-date updated" datetime="2004-07-24T18:18">Yesterday at 8:10pm</time></span>
+				</div>
+			</li>
+
+			<li>
+				<div class="author-thumb">
+					<img src="images/author-page.jpg" alt="author" class="mCS_img_loaded">
+				</div>
+				<div class="notification-event">
+					<span class="chat-message-item">Don’t worry Mathilda!</span>
+					<span class="chat-message-item">I already bought everything</span>
+					<span class="notification-date"><time class="entry-date updated" datetime="2004-07-24T18:18">Yesterday at 8:29pm</time></span>
+				</div>
+			</li>
+
+			<li>
+				<div class="author-thumb">
+					<img src="images/avatar14-sm.jpg" alt="author" class="mCS_img_loaded">
+				</div>
+				<div class="notification-event">
+					<span class="chat-message-item">Hi James! Please remember to buy the food for tomorrow! I’m gonna be handling the gifts and Jake’s gonna get the drinks</span>
+					<span class="notification-date"><time class="entry-date updated" datetime="2004-07-24T18:18">Yesterday at 8:10pm</time></span>
+				</div>
+			</li>
+		</ul>
+	</div>
+
+	<form class="need-validation">
+
+		<div class="form-group label-floating is-empty">
+			<label class="control-label">Press enter to post...</label>
+			<textarea class="form-control" placeholder=""></textarea>
+			<div class="add-options-message">
+				<a href="#" class="options-message">
+					<svg class="olymp-computer-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-computer-icon"></use></svg>
+				</a>
+				<div class="options-message smile-block">
+
+					<svg class="olymp-happy-sticker-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-happy-sticker-icon"></use></svg>
+
+					<ul class="more-dropdown more-with-triangle triangle-bottom-right">
+						<li>
+							<a href="#">
+								<img src="images/icon-chat1.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat2.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat3.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat4.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat5.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat6.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat7.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat8.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat9.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat10.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat11.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat12.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat13.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat14.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat15.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat16.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat17.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat18.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat19.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat20.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat21.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat22.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat23.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat24.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat25.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat26.png" alt="icon">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="images/icon-chat27.png" alt="icon">
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+<!-- ... end Window-popup-CHAT for responsive min-width: 768px -->
+<!-- JS Scripts -->
+<?php include("footer.php");?>
+
+</body>
+</html>
